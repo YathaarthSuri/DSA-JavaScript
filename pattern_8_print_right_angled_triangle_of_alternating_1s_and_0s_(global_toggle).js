2ca1751a841939a652903902098c6ae63bafd24b
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-20 22:43:08 +0200

    Create pattern_8_print_right_angled_triangle_of_alternating_1s_and_0s_(global_toggle).js

---
+++ pattern_8_print_right_angled_triangle_of_alternating_1s_and_0s_(global_toggle).js
@@ -0,0 +1,17 @@
+/*
+1
+01
+010
+1010
+*/
+
+let n = 4;
+let toggle = 1;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < i + 1; j++) {
+    row += toggle;
+    toggle = toggle === 1 ? 0 : 1;
+  }
+  console.log(row);
+}
