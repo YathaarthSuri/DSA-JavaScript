e16ac2e4c0e4656e4fd502afc251331fcd95497e
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-20 22:30:31 +0200

    Update check_voting_eligibility.js

--- check_voting_eligibility.js
+++ check_voting_eligibility.js
@@ -1,7 +1,7 @@
 let age = 20;
-  if (age >= 18) {
-    console.log("You are eligible to vote.");
-  } else {
-    console.log("You are not eligible to vote.");
-  }
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
       
