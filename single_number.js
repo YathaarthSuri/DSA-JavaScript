d4ebf9a10f18806445612b33849135eb5d21a616
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-20 22:51:29 +0200

    Create single_number.js

---
+++ single_number.js
@@ -0,0 +1,27 @@
+// Brute Force (Hash Map)
+
+var singleNumber = function(nums) {
+    let hash = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (!hash[nums[i]]) {
+            hash[nums[i]] = 1;
+        } else {
+            hash[nums[i]]++;
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (hash[nums[i]] === 1) {
+            return nums[i];
+        }
+    }
+};
+
+// Optimal
+
+ var singleNumber = function(nums) {
+      let xor = 0;
+      for (let i = 0; i < nums.length; i++) {
+          xor = xor ^ nums[i];
+      }
+      return xor;
+  };
