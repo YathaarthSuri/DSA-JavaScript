8aec154518c449d21a41ce5d164246f428a6f109
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-20 22:42:22 +0200

    Create pattern_7_print_right_angled_triangle_of_alternating_1s_and_0s.js

---
+++ pattern_7_print_right_angled_triangle_of_alternating_1s_and_0s.js
@@ -0,0 +1,17 @@
+/*
+1
+10
+101
+1010
+*/
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  let toggle = 1;
+  for (let j = 0; j < i + 1; j++) {
+    row += toggle;
+    toggle = toggle === 1 ? 0 : 1;
+  }
+  console.log(row);
+}
