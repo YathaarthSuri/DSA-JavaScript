be6f5685cff46577c7a8b57a205c2fa89c11a437
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-20 22:41:21 +0200

    Create pattern_6_print_right_aligned_right_angled_triangle_of_stars.js

---
+++ pattern_6_print_right_aligned_right_angled_triangle_of_stars.js
@@ -0,0 +1,18 @@
+/*
+   *
+  **
+ ***
+****
+*/
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n - (i + 1); j++) {
+    row += " ";
+  }
+  for (let k = 0; k < i + 1; k++) {
+    row += "*";
+  }
+  console.log(row);
+}
