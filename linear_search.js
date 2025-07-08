bf95c0a1dbbdd4d751e92662db2a5b96b9e95e45
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-07-08 15:02:22 +0200

    Update linear_search.js

--- linear_search.js
+++ linear_search.js
@@ -2,6 +2,6 @@ function linear_search(arr, target) {
     for(let i = 0; i < arr.length; i++) {
         if(target === arr[i]) return i;
     }
-    return -1;
+    return -1; 
 }
 console.log(linear_search([1, 4, 2, 3], 2))
